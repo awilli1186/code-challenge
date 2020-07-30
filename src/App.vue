5532450c051c944d207060756c7d114c148ce1e6
Adam Williams <awilli1186@gmail.com>
2020-07-30 15:45:05 -0500

    Clean up base project files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
     <img alt="Allday logo" src="./assets/allday-logo.svg" />
-    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// Add component imports here
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Bungee&display=swap");
 
 #app {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
-    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-family: $baseFont;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
