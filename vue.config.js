28183458cff08d7200492a5343c36ac87a655b48
Adam Williams <awilli1186@gmail.com>
2020-08-04 15:21:17 -0500

    Add svg inline loader

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,13 @@ const path = require('path')
 module.exports = {
   assetsDir: 'assets',
   chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)));
+
+    config.module
+			.rule("vue")
+			.use("vue-svg-inline-loader")
+				.loader("vue-svg-inline-loader");
   },
 }
 
