4ab37062785d06ade3afd3c48c1219e61b83cad4
Adam Williams <awilli1186@gmail.com>
2020-08-05 15:37:49 -0500

    Add dynamic styling to app when mobile menu opens

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <div id="app">
-    <Header />
+  <div id="app" :class="{active: isActive}">
+    <Header @menu-open="fixBody"/>
+    <Main />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Main,
   },
-};
+    data: function () {
+    return { isActive: false };
+  },
+  methods: {
+    fixBody: function (active) {
+      this.isActive = active;
+    }
+  }
 
+};
 </script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Bungee&display=swap");
 
 #app {
-  font-family: $baseFont;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+}
+
+.active {
+  position: fixed;
 }
 </style>
